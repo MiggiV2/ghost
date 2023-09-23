7f822942a4c65f6f690cc601eb7b3ae617e7fa84
Miggi <miggi@noreply.familyhainz.de>
2023-09-23 10:42:36 +0200

    :recycle: Using example

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,128 @@
+///
+///  This is an example showcasing how to build a very simple bot using the
+/// matrix-sdk. To try it, you need a rust build setup, then you can run:
+/// `cargo run -p example-getting-started -- <homeserver_url> <user> <password>`
+///
+/// Use a second client to open a DM to your bot or invite them into some room.
+/// You should see it automatically join. Then post `!party` to see the client
+/// in action.
+///
+/// Below the code has a lot of inline documentation to help you understand the
+/// various parts and what they do
+// The imports we need
 use std::{env, process::exit};
 
-use matrix_sdk::{Client, config::SyncSettings, RoomType, ruma::events::room::member::StrippedRoomMemberEvent};
-use matrix_sdk::room::Room;
-use matrix_sdk::ruma::{TransactionId, user_id};
-use matrix_sdk::ruma::events::room::message::{MessageType, OriginalSyncRoomMessageEvent, RoomMessageEventContent};
+use matrix_sdk::{
+    Client,
+    config::SyncSettings,
+    Room, RoomState, ruma::events::room::{
+        member::StrippedRoomMemberEvent,
+        message::{MessageType, OriginalSyncRoomMessageEvent, RoomMessageEventContent},
+    },
+};
 use tokio::time::{Duration, sleep};
 
+/// This is the starting point of the app. `main` is called by rust binaries to
+/// run the program in this case, we use tokio (a reactor) to allow us to use
+/// an `async` function run.
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // set up some simple stderr logging. You can configure it by changing the env
+    // var `RUST_LOG`
+    tracing_subscriber::fmt::init();
+
+    // parse the command line for homeserver, username and password
+    let (homeserver_url, username, password) =
+        match (env::args().nth(1), env::args().nth(2), env::args().nth(3)) {
+            (Some(a), Some(b), Some(c)) => (a, b, c),
+            _ => {
+                eprintln!(
+                    "Usage: {} <homeserver_url> <username> <password>",
+                    env::args().next().unwrap()
+                );
+                // exist if missing
+                exit(1)
+            }
+        };
+
+    // our actual runner
+    login_and_sync(homeserver_url, &username, &password).await?;
+    Ok(())
+}
+
+// The core sync loop we have running.
+async fn login_and_sync(
+    homeserver_url: String,
+    username: &str,
+    password: &str,
+) -> anyhow::Result<()> {
+    // First, we set up the client.
+
+    // Note that when encryption is enabled, you should use a persistent store to be
+    // able to restore the session with a working encryption setup.
+    // See the `persist_session` example.
+    let client = Client::builder()
+        // We use the convenient client builder to set our custom homeserver URL on it.
+        .homeserver_url(homeserver_url)
+        .build()
+        .await?;
+
+    // Then let's log that client in
+    client
+        .matrix_auth()
+        .login_username(username, password)
+        .initial_device_display_name("getting started bot")
+        .await?;
+
+    // It worked!
+    println!("logged in as {username}");
+
+    // Now, we want our client to react to invites. Invites sent us stripped member
+    // state events so we want to react to them. We add the event handler before
+    // the sync, so this happens also for older messages. All rooms we've
+    // already entered won't have stripped states anymore and thus won't fire
+    client.add_event_handler(on_stripped_state_member);
+
+    // An initial sync to set up state and so our bot doesn't respond to old
+    // messages. If the `StateStore` finds saved state in the location given the
+    // initial sync will be skipped in favor of loading state from the store
+    let sync_token = client.sync_once(SyncSettings::default()).await.unwrap().next_batch;
+
+    // now that we've synced, let's attach a handler for incoming room messages, so
+    // we can react on it
+    client.add_event_handler(on_room_message);
+
+    // since we called `sync_once` before we entered our sync loop we must pass
+    // that sync token to `sync`
+    let settings = SyncSettings::default().token(sync_token);
+    // this keeps state from the server streaming in to the bot via the
+    // EventHandler trait
+    client.sync(settings).await?; // this essentially loops until we kill the bot
+
+    Ok(())
+}
+
+// Whenever we see a new stripped room member event, we've asked our client to
+// call this function. So what exactly are we doing then?
 async fn on_stripped_state_member(
     room_member: StrippedRoomMemberEvent,
     client: Client,
     room: Room,
 ) {
     if room_member.state_key != client.user_id().unwrap() {
+        // the invite we've seen isn't for us, but for someone else. ignore
         return;
     }
 
+    // The event handlers are called before the next sync begins, but
+    // methods that change the state of a room (joining, leaving a room)
+    // wait for the sync to return the new room state so we need to spawn
+    // a new task for them.
     tokio::spawn(async move {
         println!("Autojoining room {}", room.room_id());
         let mut delay = 2;
 
-        while let Err(err) = client.join_room_by_id(room.room_id()).await {
+        while let Err(err) = room.join().await {
             // retry autojoin due to synapse sending invites, before the
             // invited user can join for more information see
             // https://github.com/matrix-org/synapse/issues/4345
@@ -37,16 +140,21 @@ async fn on_stripped_state_member(
     });
 }
 
+// This fn is called whenever we see a new room message event. You notice that
+// the difference between this and the other function that we've given to the
+// handler lies only in their input parameters. However, that is enough for the
+// rust-sdk to figure out which one to call one and only do so, when
+// the parameters are available.
 async fn on_room_message(event: OriginalSyncRoomMessageEvent, room: Room) {
-    if room.room_type() != RoomType::Joined {
+    // First, we need to unpack the message: We only want messages from rooms we are
+    // still in and that are regular text messages - ignoring everything else.
+    if room.state() != RoomState::Joined {
         return;
     }
-    let MessageType::Text(text_content) = event.content.msgtype else {
-        return;
-    };
+    let MessageType::Text(text_content) = event.content.msgtype else { return; };
 
-    println!("Got message {}", &text_content.body);
-
+    // here comes the actual "logic": when the bot see's a `!party` in the message,
+    // it responds
     if text_content.body.contains("!party") {
         let content = RoomMessageEventContent::text_plain("🎉🎊🥳 let's PARTY!! 🥳🎊🎉");
 
@@ -55,59 +163,8 @@ async fn on_room_message(event: OriginalSyncRoomMessageEvent, room: Room) {
         // send our message to the room we found the "!party" command in
         // the last parameter is an optional transaction id which we don't
         // care about.
-        let transaction_id = TransactionId::new();
-        let party_msg = matrix_sdk::ruma::api::client::message::send_message_event::v3::Request::new(
-            room.room_id(), &transaction_id, &content,
-        ).expect("Failed to create request");
-        room.client().send(party_msg, None).await.expect("Failed to send message");
-
-        // room.send(content, None).await.unwrap();
-        // client.send(content, None).await.unwrap();
+        room.send(content, None).await.unwrap();
 
         println!("message sent");
     }
-}
-
-
-async fn login_and_sync(
-    username: &str,
-    password: &str,
-) -> anyhow::Result<()> {
-    // Note that when encryption is enabled, you should use a persistent store to be
-    // able to restore the session with a working encryption setup.
-    // See the `persist_session` example.
-    let alice = user_id!("@ghost:matrix.familyhainz.de");
-    let client = Client::builder().server_name(alice.server_name()).build().await?;
-
-    // First we need to log in.
-    client.login_username(alice, password).send().await?;
-
-    println!("logged in as {username}");
-
-    client.add_event_handler(on_stripped_state_member);
-    client.add_event_handler(on_room_message);
-
-    client.sync(SyncSettings::default()).await?;
-
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
-
-    let (username, password) =
-        match (env::args().nth(1), env::args().nth(2)) {
-            (Some(a), Some(b)) => (a, b),
-            _ => {
-                eprintln!(
-                    "Usage: {} <username> <password>",
-                    env::args().next().unwrap()
-                );
-                exit(1)
-            }
-        };
-
-    login_and_sync(&username, &password).await?;
-    Ok(())
 }
