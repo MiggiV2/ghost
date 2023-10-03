f8cf231f407b1911dcd8e47ca9aaa92b43e351d6
Miggi <miggi@noreply.familyhainz.de>
2023-10-03 19:12:19 +0200

    :sparkles: Very simple ping command

--- src/handler/new_room_msg.rs
+++ src/handler/new_room_msg.rs
@@ -1,8 +1,5 @@
-use std::time::Duration;
-
 use matrix_sdk::{Room, RoomState};
 use matrix_sdk::ruma::events::room::message::{MessageType, OriginalSyncRoomMessageEvent, RoomMessageEventContent};
-use tokio::time::sleep;
 
 use crate::handler::send_startup_msg::build_health_message;
 use crate::status_checker::config_builder::ConfBuilder;
@@ -13,25 +10,11 @@ pub async fn on_room_message(event: OriginalSyncRoomMessageEvent, room: Room) {
     }
     let MessageType::Text(text_content) = event.content.msgtype else { return; };
 
-    if text_content.body.contains("!party") {
-        let content = RoomMessageEventContent::text_plain("🎉🎊🥳 let's PARTY!! 🥳🎊🎉");
+    if text_content.body.contains("!ping") {
+        let content = RoomMessageEventContent::text_plain("Hi 🥹 It's me!");
         println!("sending");
         room.send(content, None).await.unwrap();
         println!("message sent");
-    }
-
-    if text_content.body.contains("!sleep") {
-        tokio::spawn(async move {
-            let delay = 20;
-            println!("Starting future task...");
-            println!("Sleeping for {} sec...", delay);
-            sleep(Duration::from_secs(delay)).await;
-            println!("Ahhh, good power nap!\nSending msg...");
-            let _content = RoomMessageEventContent::text_plain("Good morning! 😊");
-            eprintln!("Disabled!");
-            // room.send(content, None).await.expect("Failed to send msg!");
-            println!("Message sent!");
-        });
     }
 
     if text_content.body.contains("!health") {
